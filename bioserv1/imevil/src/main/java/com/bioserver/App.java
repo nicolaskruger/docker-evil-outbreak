7b08b39d74eeae84dd8ae0a836be6e7f8ed63e5a
nicolas <nicolas.nkruge@gmaila.com>
2025-04-13 20:25:16 -0300

    :fire:

---
+++ bioserv1/imevil/src/main/java/com/bioserver/App.java
@@ -0,0 +1,13 @@
+package com.bioserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "devil don't cry" );
+    }
+}
