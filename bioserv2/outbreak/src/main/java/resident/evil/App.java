bf390ebdf6a8827f826f541d8b66d4324e2ca3d4
nkruger <nicolas.nkruger@gmail.com>
2025-05-01 11:17:40 -0300

    :fire:

---
+++ bioserv2/outbreak/src/main/java/resident/evil/App.java
@@ -0,0 +1,13 @@
+package resident.evil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "🧟‍♂️🧟‍♀️🔫🧬🧪🏥🕵️‍♂️🧧🚪🩸🔍💉☣️💀🌧️🔦" );
+    }
+}
